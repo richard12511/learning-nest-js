920263e8c7c5e4ece61c0945001e1995e641fba9
Richard Schmidt <richardnew12511@gmail.com>
2022-06-03 19:02:05 -0500

    first commit

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.log('before request');
+    return next.handle().pipe(
+      map((data: any) => {
+        console.log('after request');
+      }),
+    );
+  }
+}
